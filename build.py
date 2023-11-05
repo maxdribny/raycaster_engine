991cbd23980caf35e63c0c318d1ab038aec8a082
Max <mdribnyi@gmail.com>
2023-11-05 18:06:14 +0400

    Updated build.py to run independently

--- build.py
+++ build.py
@@ -4,18 +4,12 @@ import subprocess
 
 # Build flags
 FLAGS = {
-    'IS_BUILD_VERSION': False,
     'VERSION': '0.0.1'
 }
 
 print(f'Building version {FLAGS["VERSION"]}')
-print(f'FLAGS = {FLAGS}')
 
-# Check if IS_BUILD_VERSION is set to False
-if FLAGS['IS_BUILD_VERSION']:
-    command = 'pyinstaller'
-    params = ['--onefile', '--log-level', 'TRACE', 'main.py']
+command = 'pyinstaller'
+params = ['--onefile', '--log-level', 'TRACE', 'main.py']
 
-    subprocess.run([command] + params)
-else:
-    print(f'Not building because IS_BUILD_VERSION is set to {FLAGS["IS_BUILD_VERSION"]}')
+subprocess.run([command] + params)
