f4ef3574aa6302402173387d5d09bf9d0ad70c6a
Max <mdribnyi@gmail.com>
2023-11-05 18:37:37 +0400

    Exclude pyopengl and add modify build params

--- build.py
+++ build.py
@@ -9,7 +9,9 @@ FLAGS = {
 
 print(f'Building version {FLAGS["VERSION"]}')
 
+output_name = f'Raycaster_v{FLAGS["VERSION"]}'
+
 command = 'pyinstaller'
-params = ['--log-level', 'TRACE', 'main.py']
+params = ['--log-level', 'TRACE', f'--name={output_name}', 'main.py']
 
 subprocess.run([command] + params)
