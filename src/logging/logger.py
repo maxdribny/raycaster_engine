621468aa5a27bb105a33da331155ba0b7e857b6b
Max <mdribnyi@gmail.com>
2023-11-05 18:03:16 +0400

    Added logger.py

--- src/logging/logger.py
+++ src/logging/logger.py
@@ -8,3 +8,29 @@ class Logger:
         :param debug_mode: If true, debug messages will be printed. Otherwise, they will be ignored.
         """
         self.debug_mode = debug_mode
+
+    def log(self, message: str):
+        """
+        Log a message with the 'LOG' level.
+
+        :param message: The message to be logged.
+        """
+        self._print_message("LOG", message)
+
+    def debug(self, message: str):
+        """
+        Log a message with the 'DEBUG' level.
+
+        :param message: The message to be logged.
+        """
+        if self.debug_mode:
+            self._print_message("DEBUG", message)
+
+    def _print_message(self, level: str, message: str):  # noqa
+        """
+        Print a message to the console.
+
+        :param level: The level of the message.
+        :param message: The message to be printed.
+        """
+        print(f"{level}: {message}")
